cdbdc450b0aeb61a389eafaac051bd5635540c37
Nathan Shin <mastertofu12345@gmail.com>
2024-09-08 13:19:26 -0400

    feat: add join discord server button

--- places/2440500124.lua
+++ places/2440500124.lua
@@ -2302,6 +2302,14 @@ MenuGroup:AddToggle("KeybindMenuOpen", { Default = false, Text = "Open Keybind M
 MenuGroup:AddToggle("ShowCustomCursor", {Text = "Custom Cursor", Default = true, Callback = function(Value) Library.ShowCustomCursor = Value end})
 MenuGroup:AddDivider()
 MenuGroup:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind", { Default = "RightShift", NoUI = true, Text = "Menu keybind" })
+MenuGroup:AddButton("Join Discord Server", function()
+    local Inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+	Inviter.Join("https://discord.com/invite/cfyMptntHr")
+	Inviter.Prompt({
+		name = "mspaint",
+		invite = "https://discord.com/invite/cfyMptntHr",
+	})
+end)
 MenuGroup:AddButton("Unload", function() Library:Unload() end)
 
 CreditsGroup:AddLabel("deividcomsono - script dev")
