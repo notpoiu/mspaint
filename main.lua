65901688e99759295e3b88dcca42d6f10308834b
Nathan Shin <mastertofu12345@gmail.com>
2024-09-08 13:33:05 -0400

    refactor: remove commented ENV thing

--- main.lua
+++ main.lua
@@ -1,8 +1,1 @@
---[[
-local ENV = "DEV"
-
-if ENV == "DEV" then
-    loadstring(game:HttpGet(""))
-end]]
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/places/" .. game.GameId .. ".lua"))()
