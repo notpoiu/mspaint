5354cbc6655b9e225e9f093cc77e9be552c6a0a9
Hoangnguyen72628 <96629210+Hoangnguyen72628@users.noreply.github.com>
2024-09-10 19:07:38 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,1 @@
---[[
-local ENV = "DEV"
-
-if ENV == "DEV" then
-    loadstring(game:HttpGet(""))
-end]]
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Hoangnguyen72628/mspaint/Hoangnguyen72628-patch-1/places/" .. game.GameId .. ".lua"))()
