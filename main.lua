e3cb133bfb5178317218722db1ce710c367bae6c
Nathan Shin <mastertofu12345@gmail.com>
2024-09-11 14:12:58 -0400

    feat: add support for other executors that are like solara

--- main.lua
+++ main.lua
@@ -1,4 +1,40 @@
-local ScriptBranch = "main"
+local compatibility_mode = false do
+    local executor_name = identifyexecutor and identifyexecutor() or "your executor"
 
-if identifyexecutor and identifyexecutor() == "Solara" then ScriptBranch = "solara" end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/" .. ScriptBranch .. "/places/" .. game.GameId .. ".lua"))()
+    -- Known executors that do not support the functions
+    if executor_name == "Solara" then compatibility_mode = true end
+
+    function test(name: string, func: () -> (), ...)
+        if compatibility_mode then return end
+
+        local success, error_msg = pcall(func, ...)
+        
+        if not success then
+            compatibility_mode = true
+            print("mspaint: " .. executor_name .. " does not support " .. name .. ", falling back to compatibility mode")
+        end
+        
+        return success
+    end
+        
+    test("require", function() require(game:GetService("ReplicatedStorage"):FindFirstChildWhichIsA("ModuleScript", true)) end)
+    test("hookmetamethod", function()
+        -- From UNC Env Check
+        local object = setmetatable({}, { __index = newcclosure(function() return false end), __metatable = "Locked!" })
+        local ref = hookmetamethod(object, "__index", function() return true end)
+        assert(object.test == true, "Failed to hook a metamethod and change the return value")
+        assert(ref() == false, "Did not return the original function")
+        
+        local method, ref; ref = hookmetamethod(game, "__namecall", function(...)
+            if not method then
+                method = getnamecallmethod()
+            end
+            return ref(...)
+        end)
+        
+        game:GetService("Lighting")
+        assert(method == "GetService", "Did not get the correct method (GetService)")
+    end)
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/" .. (compatibility_mode and "solara" or "main") .. "/places/" .. game.GameId .. ".lua"))()
