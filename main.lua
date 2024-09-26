c9440a1c545ce142645aade156f9050b153b8a6c
deividcomsono <116926351+deividcomsono@users.noreply.github.com>
2024-09-25 22:07:31 -0300

    feat: unc test for main branch

--- main.lua
+++ main.lua
@@ -1,106 +1,124 @@
-local compatibility_mode = false do
-    local executor_name = identifyexecutor and identifyexecutor() or "your executor"
+if not ExecutorSupport then
+    local Workspace = game:GetService("Workspace")
 
-    -- Known executors that do not support the functions
-    if executor_name == "Solara" then compatibility_mode = true end
+    local executorSupport = {}
 
-    -- Function replacement
-    if not fireproximityprompt or executor_name == "Solara" then
-        function newinstance(itype, iparent, iproperties)
-            if not itype or typeof(itype) ~= 'string' then return; end
-            if typeof(iparent) ~= 'Instance' then iparent = nil; end
-
-            local i = Instance.new(itype, iparent)
-            if iproperties and typeof(iproperties) == 'table' then
-                for property, value in pairs(iproperties) do
-                    pcall(function() i[property] = value end)
-                end
-            end
-
-            return i
-        end
-        
-        function posprompt(prompt, enabled)
-            local camcf, part = workspace.CurrentCamera.CFrame, nil
-            if prompt.Parent.Name == '_pxpart' then part = prompt.Parent; end
-            if prompt.Parent:FindFirstChild('_pxpart') then part = prompt.Parent:FindFirstChild('_pxpart'); end
-            if not part then
-                part = newinstance('Part', prompt.Parent, {Name = '_pxpart', Transparency = 1, CanCollide = false, CanTouch = false, CanQuery = false, Size = Vector3.new(0.5, 0.5, 0.5), Shape = 'Ball', Anchored = true})
-            end
-            part.CFrame = camcf + (camcf.lookVector * 1.4)
-            if enabled == true then prompt.Parent = part; end
-            if enabled == false and prompt.Parent.Name == '_pxpart' then prompt.Parent = prompt.Parent.Parent; end
-        end
-        
-        function _fireproxp(obj)
-            local oldenabled, oldrlos = obj.Enabled, obj.RequiresLineOfSight
-            obj.Enabled = true; obj.RequiresLineOfSight = false
-            local PromptTime = obj.HoldDuration
-            obj.HoldDuration = 0; posprompt(obj, true)
-            task.wait()
-            obj:InputHoldBegin()
-            obj:InputHoldEnd()
-            posprompt(obj, false); obj.HoldDuration = PromptTime; obj.Enabled = oldenabled; obj.RequiresLineOfSight = oldrlos
-        end
-        
-        fireproximityprompt = _fireproxp
-        getgenv().fireproximityprompt = _fireproxp
-    end
-    
     function test(name: string, func: () -> (), ...)
-        if compatibility_mode then return end
-
-        local success, error_msg = pcall(func, ...)
+        local success, _ = pcall(func, ...)
         
-        if not success then
-            compatibility_mode = true
-            print("mspaint: " .. executor_name .. " does not support " .. name .. ", falling back to compatibility mode")
-        end
+        executorSupport[name] = success
         
         return success
     end
-        
-    test("require", function() require(game:GetService("ReplicatedStorage"):FindFirstChildWhichIsA("ModuleScript", true)) end)
+
+    test("require", require, game:GetService("ReplicatedStorage"):WaitForChild("ModuleScript"))
     test("hookmetamethod", function()
-        -- From UNC Env Check
         local object = setmetatable({}, { __index = newcclosure(function() return false end), __metatable = "Locked!" })
         local ref = hookmetamethod(object, "__index", function() return true end)
         assert(object.test == true, "Failed to hook a metamethod and change the return value")
         assert(ref() == false, "Did not return the original function")
-        
-        local method, ref; ref = hookmetamethod(game, "__namecall", function(...)
+
+        local method
+        local ref
+        ref = hookmetamethod(game, "__namecall", function(...)
             if not method then
                 method = getnamecallmethod()
             end
             return ref(...)
         end)
-        
         game:GetService("Lighting")
         assert(method == "GetService", "Did not get the correct method (GetService)")
     end)
-
     test("firesignal", function()
         local event = Instance.new("BindableEvent")
-        
         local fired = false
-        event.Event:Connect(function(value) fired = value end)
 
-        if firesignal then
-            firesignal(event.Event, true)
-            task.wait()
-        end
-        
-        if not fired then
-            for _, connection in pairs(getconnections(event.Event)) do
-                connection:Fire(true)
-            end
+        event.Event:Once(function(value)
+            fired = value
+        end)
 
-            task.wait()
-        end
+        firesignal(event.Event, true)
 
+        task.wait()
         event:Destroy()
+
         assert(fired, "Failed to fire a BindableEvent")
     end)
+    local canFirePrompt = test("fireproximityprompt", function()
+        local prompt = Instance.new("ProximityPrompt", Instance.new("Part", Workspace))
+        local triggered = false
+
+        prompt.Triggered:Once(function()
+            triggered = true
+        end)
+
+        fireproximityprompt(prompt)
+        task.wait(0.1)
+
+        prompt.Parent:Destroy()
+        assert(triggered, "Failed to fire proximity prompt")
+        print("finished this one")
+    end)
+
+    --// Fixes \\--
+
+    if not canFirePrompt then
+        getgenv().fireproximityprompt = function(prompt: ProximityPrompt, lookToPrompt: boolean)
+            if not prompt:IsA("ProximityPrompt") then
+                return error("ProximityPrompt expected, got " .. typeof(prompt))
+            end
+
+            local connection
+            local promptPosition = prompt.Parent:GetPivot().Position
+        
+            local originalEnabled = prompt.Enabled
+            local originalHold = prompt.HoldDuration
+            local originalLineOfSight = prompt.RequiresLineOfSight
+            local originalCamCFrame = workspace.CurrentCamera.CFrame
+        
+            prompt.Enabled = true
+            prompt.HoldDuration = 0
+            prompt.RequiresLineOfSight = false
+            
+            if lookToPrompt then
+                workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, promptPosition)
+                connection = workspace.CurrentCamera:GetPropertyChangedSignal("CFrame"):Connect(function()
+                    workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, promptPosition)
+                end)
+
+                task.wait()
+            end
+
+            prompt:InputHoldEnd()
+            prompt:InputHoldBegin()
+            prompt:InputHoldEnd()
+
+            if connection then connection:Disconnect() end
+
+            prompt.Enabled = originalEnabled
+            prompt.HoldDuration = originalHold
+            prompt.RequiresLineOfSight = originalLineOfSight
+            workspace.CurrentCamera.CFrame = originalCamCFrame
+        end
+    end
+
+    if not isnetworkowner then
+        getgenv().isnetworkowner = function(part: BasePart)
+            if not part:IsA("BasePart") then
+                return error("BasePart expected, got " .. typeof(part))
+            end
+
+            return part.ReceiveAge == 0
+        end
+    end
+
+    --// Load \\--
+
+    for name, result in pairs(executorSupport) do
+        print(name .. ":", result)
+    end
+
+    getgenv().ExecutorSupport = executorSupport
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/" .. (compatibility_mode and "solara" or "main") .. "/places/" .. game.GameId .. ".lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/places/" .. game.GameId .. ".lua"))()
