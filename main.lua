01e0401112638ba4b09e040dc4c3cb8ad5d4eb6c
Luna-Python-Coder <157140569+Fuydutdtu@users.noreply.github.com>
2024-09-30 08:45:17 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local HttpService = game:GetService("HttpService")
-local baseURL = "https://raw.githubusercontent.com/notpoiu/mspaint/main"
+local baseURL = "https://github.com/Fuydutdtu/mspaintv3omg/blob/main"
 
 export type gameMapping = {
     exclusions: table?,
