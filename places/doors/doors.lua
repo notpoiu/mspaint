69fca9dfd4106ee2b09931fa24c55b2d117ac880
Nathan Shin <mastertofu12345@gmail.com>
2024-10-03 17:40:59 -0400

    feat: Custom Addons
    Co-authored-by: mstudio45 <mstudio45@users.noreply.github.com>

--- places/doors/doors.lua
+++ places/doors/doors.lua
@@ -5929,3 +5929,134 @@ SaveManager:LoadAutoloadConfig()
 
 Script.Functions.UpdateRPC()
 getgenv().mspaint_loaded = true
+
+-- Addons (this is BETA, expect stuff to change) --
+if typeof(isfolder) == "function" and typeof(isfile) == "function" then
+    if isfolder("mspaint\\doors\\addons") then
+        local function AddAddonElement(LinoriaElement, AddonName, Element)
+            if not LinoriaElement then
+                warn("[mspaint] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid Linoria element.")
+                return
+            end
+
+            if typeof(Element) ~= "table" then
+                warn("[mspaint] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid data.")
+                return
+            end 
+
+            if typeof(Element.Type) ~= "string" then 
+                warn("[mspaint] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid name.")
+                return 
+            end
+
+            if typeof(AddonName) ~= "string" then 
+                warn("[mspaint] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid addon name.")
+                return 
+            end
+
+            if Element.Type:sub(1, 3) == "Add" then Element.Type = Element.Type:sub(4) end
+
+            -- Elements with no Arguments
+            if Element.Type == "Divider" then
+                return LinoriaElement:AddDivider()
+            end
+
+            if Element.Type == "DependencyBox" then
+                return LinoriaElement:AddDependencyBox()
+            end
+
+            if typeof(Element.Name) ~= "string" then 
+                warn("[mspaint] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid name.")
+                return 
+            end
+
+            -- Elements with Arguments
+            if typeof(Element.Arguments) == "table" then
+                if Element.Type == "Label" then
+                    return LinoriaElement:AddLabel(table.unpack(Element.Arguments))
+                end
+
+                if Element.Type == "Toggle" then
+                    return LinoriaElement:AddToggle(AddonName .. "_" .. Element.Name, Element.Arguments)
+                end
+                
+                if Element.Type == "Button" then
+                    return LinoriaElement:AddButton(Element.Arguments)
+                end
+                
+                if Element.Type == "Slider" then
+                    return LinoriaElement:AddSlider(AddonName .. "_" .. Element.Name, Element.Arguments)
+                end
+                
+                if Element.Type == "Input" then
+                    return LinoriaElement:AddInput(AddonName .. "_" .. Element.Name, Element.Arguments)
+                end
+                
+                if Element.Type == "Dropdown" then
+                    return LinoriaElement:AddInput(AddonName .. "_" .. Element.Name, Element.Arguments)
+                end
+                
+                if Element.Type == "ColorPicker" then
+                    return LinoriaElement:AddColorPicker(AddonName .. "_" .. Element.Name, Element.Arguments)        
+                end
+                
+                if Element.Type == "KeyPicker" then
+                    return LinoriaElement:AddKeyPicker(AddonName .. "_" .. Element.Name, Element.Arguments)
+                end
+            end
+
+            warn("[mspaint] Element '" .. tostring(Element.Name) .. " (" .. tostring(Element.Type) .. ")' didn't load: Invalid element type.")
+        end
+        local LastGroupbox = "Right" -- do not change this to something else then Right PLEASE
+
+        for _, file in pairs(listfiles("mspaint\\doors\\addons")) do
+            if file:sub(#file - 3) == ".lua" or file:sub(#file - 4) == ".luau" then
+                local success, errorMessage = pcall(function()
+                    local fileContent = readfile(file)
+                    local addon = loadstring(fileContent)()
+
+                    if typeof(addon.Name) ~= "string" or typeof(addon.Elements) ~= "table" then
+                        warn("Addon '" .. string.gsub(file, "mspaint/doors/addons/", "") .. "' didn't load: Invalid Name/Elements.")
+                        return 
+                    end
+                    addon.Name = addon.Name:gsub("%s+", "")
+                    if typeof(addon.Title) ~= "string" then
+                        addon.Title = addon.Name;
+                    end
+                    
+                    if not Tabs["Addons"] then
+                        Tabs.Addons = Window:AddTab("Addons [BETA]")
+                        Tabs.Addons:UpdateWarningBox({
+                            Visible = true,
+                            Title = "WARNING",
+                            Text = "This tab is for UN-OFFICIAL addons made for mspaint. We are not responsible for what addons you will use. You are putting yourself AT RISK since you are executing third-party scripts."
+                        })
+                    end
+
+                    local AddonGroupbox = LastGroupbox == "Right" and Tabs.Addons:AddLeftGroupbox(addon.Title) or Tabs.Addons:AddRightGroupbox(addon.Title);
+                    LastGroupbox = LastGroupbox == "Right" and "Left" or "Right";
+                    if typeof(addon.Description) == "string" then
+                        AddonGroupbox:AddLabel(addon.Description, true)
+                    end
+
+                    local function loadElements(linoriaMainElement, elements)
+                        for _, element in pairs(elements) do
+                            task.spawn(function()
+                                local linoriaElement = AddAddonElement(linoriaMainElement, addon.Name, element)
+                                if linoriaElement ~= nil and typeof(element.Elements) == "table" then
+                                    loadElements(linoriaElement, element.Elements)
+                                end  
+                            end)                      
+                        end
+                    end
+
+                    loadElements(AddonGroupbox, addon.Elements)
+                end)
+
+                if not success then
+                    warn("[mspaint] Failed to load addon '" .. string.gsub(file, "mspaint/doors/addons/", "") .. "':", errorMessage)
+                end
+            end
+        end
+    end
+end
