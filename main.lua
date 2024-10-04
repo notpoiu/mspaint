099004c9f191504cda659b97e6cfe9ed3bfb0907
Dylan3020 <144632565+Dylan3020@users.noreply.github.com>
2024-10-04 16:22:58 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local HttpService = game:GetService("HttpService")
-local baseURL = "https://raw.githubusercontent.com/notpoiu/mspaint/main"
+local baseURL = "https://raw.githubusercontent.com/Dylan3020/mspaint/main"
 
 export type gameMapping = {
     exclusions: table?,
