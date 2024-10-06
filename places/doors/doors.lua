d7d45ac02537d135794298896561acf100aa8c7e
Nathan Shin <mastertofu12345@gmail.com>
2024-10-06 10:55:24 -0400

    fix: change delay to 0.2

--- places/doors/doors.lua
+++ places/doors/doors.lua
@@ -5024,7 +5024,7 @@ Library:GiveSignal(ProximityPromptService.PromptTriggered:Connect(function(promp
         local toolId = equippedTool and equippedTool:GetAttribute("ID")
 
         if Toggles.InfItems.Value and equippedTool and equippedTool:GetAttribute("UniversalKey") then
-            task.wait(isChestBox and 0.25 or 0)
+            task.wait(isChestBox and 0.2 or 0)
             remotesFolder.DropItem:FireServer(equippedTool)
 
             task.spawn(function()
