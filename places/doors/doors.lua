0505ca0cf2dc51a6ed967c03c160bec865daec39
deividcomsono <116926351+deividcomsono@users.noreply.github.com>
2024-10-09 18:42:32 -0300

    fix: auto interact check

--- places/doors/doors.lua
+++ places/doors/doors.lua
@@ -5956,10 +5956,10 @@ Library:GiveSignal(RunService.RenderStepped:Connect(function()
                 if Options.AutoInteractIgnore.Value["Jeff Items"] and prompt.Parent:GetAttribute("JeffShop") then return false end
                 if Options.AutoInteractIgnore.Value["Unlock w/ Lockpick"] and (prompt.Name == "UnlockPrompt" or prompt.Parent:GetAttribute("Locked")) and character:FindFirstChild("Lockpick") then return false end
                 if Options.AutoInteractIgnore.Value["Gold"] and prompt.Name == "LootPrompt" then return false end
-                if Options.AutoInteractIgnore.Value["Light Source Items"] and prompt.Parent:GetAttribute("Tool_LightSource") then return false end
+                if Options.AutoInteractIgnore.Value["Light Source Items"] and prompt.Parent:GetAttribute("Tool_LightSource") and not prompt.Parent:GetAttribute("Tool_CanCutVines") then return false end
                 if Options.AutoInteractIgnore.Value["Skull Prompt"] and prompt.Name == "SkullPrompt" then return false end
 
-                if prompt.Parent:GetAttribute("PropType") == "Battery" and (character:FindFirstChildOfClass("Tool") and (character:FindFirstChildOfClass("Tool"):GetAttribute("RechargeProp") ~= "Battery" or character:FindFirstChildOfClass("Tool"):GetAttribute("StorageProp") ~= "Battery")) then return false end 
+                if prompt.Parent:GetAttribute("PropType") == "Battery" and not (character:FindFirstChildOfClass("Tool") and (character:FindFirstChildOfClass("Tool"):GetAttribute("RechargeProp") == "Battery" or character:FindFirstChildOfClass("Tool"):GetAttribute("StorageProp") == "Battery")) then return false end 
                 if prompt.Parent:GetAttribute("PropType") == "Heal" and humanoid and humanoid.Health == humanoid.MaxHealth then return false end
                 if prompt.Parent.Name == "MinesAnchor" then return false end
 
